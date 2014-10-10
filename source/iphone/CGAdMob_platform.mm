fdecf7458996ae6ca530606b14cb0bda498b99bd
agramonte <adriangomez@adolfogmezsmbp2.attlocal.net>
2014-10-10 00:19:26 -0400

    Attempt to fix view null issue.

--- source/iphone/CGAdMob_platform.mm
+++ source/iphone/CGAdMob_platform.mm
@@ -7,8 +7,11 @@
  * be overwritten (unless --force is specified) and is intended to be modified.
  */
 #include "CGAdMob_internal.h"
-#include "GADAdSize.h"
-#include "GADBannerView.h"
+
+extern "C"{
+#import "GADBannerView.h"
+}
+
 #include "GADBannerViewDelegate.h"
 #include "GADInterstitial.h"
 #include "GADInterstitialDelegate.h"
@@ -19,6 +22,8 @@
 #include "s3eEdk_iphone.h"
 #include "s3eDebug.h"
 
+
+
 #import <UIKit/UIKit.h>
 
 
@@ -27,14 +32,76 @@
 //========================= ViewController ===============================//
 //===========================================================================//
 
-@interface CGAdMob : NSObject
+@interface CGAdMob : NSObject<GADInterstitialDelegate>
 @property (nonatomic, strong) GADInterstitial *interstitial;
 @property (nonatomic, strong) GADBannerView  *bannerView;
 @property (nonatomic) const char *interstatialAdId;
 @property (nonatomic, retain) NSString *deviceHashId;
+@property (nonatomic) bool interstitialError;
 @end
 
 @implementation CGAdMob
+
+
+/// Called when an interstitial ad request succeeded. Show it at the next transition point in your
+/// application such as when transitioning between view controllers.
+- (void)interstitialDidReceiveAd:(GADInterstitial *)ad
+{
+    
+}
+
+/// Called when an interstitial ad request completed without an interstitial to
+/// show. This is common since interstitials are shown sparingly to users.
+- (void)interstitial:(GADInterstitial *)ad
+    didFailToReceiveAdWithError:(GADRequestError *)error{
+    
+    NSString *info;
+    info = [NSString stringWithFormat:
+            @"--------------------------------------- Failed to receive interstitial ad."];
+    
+
+    s3eDebugOutputString([info UTF8String]);
+
+    self.interstitialError = true;
+
+}
+
+
+/// Called just before presenting an interstitial. After this method finishes the interstitial will
+/// animate onto the screen. Use this opportunity to stop animations and save the state of your
+/// application in case the user leaves while the interstitial is on screen (e.g. to visit the App
+/// Store from a link on the interstitial).
+- (void)interstitialWillPresentScreen:(GADInterstitial *)ad{
+    
+}
+
+/// Called before the interstitial is to be animated off the screen.
+- (void)interstitialWillDismissScreen:(GADInterstitial *)ad{
+    
+}
+
+/// Called just after dismissing an interstitial and it has animated off the screen.
+- (void)interstitialDidDismissScreen:(GADInterstitial *)ad{
+    
+    NSString *info;
+    info = [NSString stringWithFormat:
+            @"--------------------------------------- Interstitial dismissed."];
+    
+
+    s3eDebugOutputString([info UTF8String]);
+
+    s3eEdkGetUIViewController().view = s3eEdkGetUIView();
+    
+}
+
+/// Called just before the application will background or terminate because the user clicked on an
+/// ad that will launch another application (such as the App Store). The normal
+/// UIApplicationDelegate methods, like applicationDidEnterBackground:, will be called immediately
+/// before this.
+- (void)interstitialWillLeaveApplication:(GADInterstitial *)ad{
+    
+}
+
 
 
 -(void)loadInterstitialRequest{
@@ -42,6 +109,7 @@
     GADRequest *request = [GADRequest request];
     request.testDevices = @[ self.deviceHashId];
     [self.interstitial loadRequest:request];
+    self.interstitial.delegate = self;
 }
 
 -(void)loadBannerRequest{
@@ -67,19 +135,28 @@
 -(void)showInterstitial{
     
     NSString *info;
+    
     if ([self.interstitial isReady]) {
         
         info = [NSString stringWithFormat:
                 @"--------------------------------------- Interstitial is Ready."];
         
         [self.interstitial presentFromRootViewController:s3eEdkGetUIViewController()];
+        self.interstitialError = false;
+        
         
         self.initInterstitial;
         self.loadInterstitialRequest;
         
-    } else
-    {
+    } else if (self.interstitialError) {
         
+        self.initInterstitial;
+        self.loadInterstitialRequest;
+        
+        info = [NSString stringWithFormat:
+                @"------------------------Last request failed calling new one."];
+        
+    } else {
         info = [NSString stringWithFormat:
                 @"--------------------------------------- Interstitial not Ready."];
     }
@@ -98,8 +175,7 @@ CGAdMob *intAd = [[CGAdMob alloc] autorelease];
 const char* _bannerAdUnitId = "";
 const char* _interstatialAdUnitId = "";
 bool _landscape = false;
-CGPoint _point;
-bool _pointSet = false;
+bool _displayBannerAdBottom = false;
 
 
 s3eResult CGAdMobInit_platform()
@@ -117,19 +193,29 @@ void InitAdView_platform()
     //Start the banner ads
     if (_landscape) {
         
-        if (_pointSet) {
-            intAd.bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerLandscape origin:(CGPoint)_point];
-        }else
-        {
+        if (_displayBannerAdBottom) {
+            
+            CGSize adSize = CGSizeFromGADAdSize(kGADAdSizeSmartBannerLandscape);
+            
+            CGPoint origin = CGPointMake(0.0, s3eEdkGetUIViewController().view.frame.size.height - adSize.height);
+            
+            intAd.bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerLandscape origin:(CGPoint)origin];
+        }else{
             intAd.bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerLandscape];
         }
         
     } else
     {
-        if (_pointSet) {
-            intAd.bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait origin:(CGPoint)_point];
+        if (_displayBannerAdBottom) {
+            
+            CGSize adSize = CGSizeFromGADAdSize(kGADAdSizeSmartBannerPortrait);
+            
+            CGPoint origin = CGPointMake(0.0, s3eEdkGetUIViewController().view.frame.size.height - adSize.height);
+            
+            intAd.bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait origin:origin];
+            
         } else {
-        intAd.bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait];
+            intAd.bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait];
         }
     }
     
@@ -180,9 +266,7 @@ void IsLandscape_platform(bool landscape)
 
 void BannerAdPosition_platform(int x, int y)
 {
-    _point.x = x;
-    _point.y = y;
-    _pointSet = true;
+    _displayBannerAdBottom = true;
 }
 
 void TestDeviceHashedId_platform(const char* deviceHashId)
